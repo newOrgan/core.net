b32f70fb0bbafcdfcca7777365a62e1a3500df86
superTemir <akirabenj@hotmail.com>
2016-12-09 10:43:55 +0300

    1.0.1

--- src/DeliveryApp/Startup.cs
+++ src/DeliveryApp/Startup.cs
@@ -33,9 +33,9 @@ namespace DeliveryApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             // Add framework services.
             services.AddApplicationInsightsTelemetry(Configuration);
-
             services.AddMvc();
         }
 
