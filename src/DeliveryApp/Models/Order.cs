30deabeeb690b2159fcaa902fe43b2b8f1d1fac5
uchuldum <uchuldum@gmail.com>
2016-12-09 14:52:24 +0700

    modelOrder

---
+++ src/DeliveryApp/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DeliveryApp.Models;
+
+namespace DeliveryApp.Models
+{
+    public class Order
+    {
+        public int order_id { get; set; }
+        public string order_name { get; set; } // имя фамилия покупателя
+        public string order_adress { get; set; } // адрес покупателя
+        public string order_phone { get; set; } // контактный телефон покупателя
+        public int order_key { get; set; }
+        public Dishes Dishes { get; set; }
+    }
+}
